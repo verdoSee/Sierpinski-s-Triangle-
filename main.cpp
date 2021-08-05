4d39374c8e4afacbab2765fb79675031d226dae8
verdoSee <65507003+verdoSee@users.noreply.github.com>
2021-08-05 22:59:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(960, 540), "SFML tutorial", sf::Style::None);
     sf::RenderTexture texture;
-    srand(time(0));
 
     if (!texture.create(960, 540))
         return -1;
+
+    srand(time(0));
 
     int x[3] = {460, 0, 960};
     int y[3] = {0, 540, 540};
@@ -15,11 +16,12 @@ int main() {
     float last_y = 540;
 
     texture.clear(sf::Color::Black);
+    sf::Sprite sprite(texture.getTexture());
 
-    while (window.isOpen()) {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) 
-            window.close();   
+    int iterations = 1000000; //You can change this to whatever value you want... More value more detail but after some point it remains the same
+    int i = 0;
 
+    while (i < iterations) {
         int dice = rand() % 3;
 
         if (last_x >= x[dice])
@@ -35,10 +37,29 @@ int main() {
         sf::Vertex point(sf::Vector2f(last_x, last_y), sf::Color::Green);
         texture.draw(&point, 1, sf::Points);
         texture.display();
-        window.clear();
         sf::Sprite sprite(texture.getTexture());
-        window.draw(sprite);
-        window.display();
+        i++;
+        if (i == iterations / 4)
+            std::cout << "25%" << std::endl;
+        else if (i == iterations / 2)
+            std::cout << "50%" << std::endl;
+        else if (i == int(iterations / 1.33))
+            std::cout << "75%" << std::endl;
+        else if (i == iterations)
+            std::cout << "100%" <<std:: endl;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(960, 540), "Fractal", sf::Style::Close);
+
+    window.clear();
+    window.draw(sprite);
+    window.display();
+
+    while (window.isOpen()) {
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
+            if (evnt.type == evnt.Closed)
+                window.close();
     }
 
     return 0;
